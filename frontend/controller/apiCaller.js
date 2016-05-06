996979cfe6e280b75b1adb25dced92ee04e0b3d4
michal.sevcik@atos.net <michal.sevcik@atos.net>
2016-05-06 10:41:53 +0200

    method fix from prototype to normal method, callback on api call

--- frontend/controller/apiCaller.js
+++ frontend/controller/apiCaller.js
@@ -4,21 +4,20 @@
 var querystring = require('querystring');
 var https = require('https');
 
-var host = 'developers.zomato.com/api/v2.1';
+var host = 'developers.zomato.com';
 var apiKey = 'c8c3348fe90d2d59d73cb8f4bde9c8f6';
-var sessionId = null;
+var apiHeader = "user-key";
 
 function apiCaller(id){
     this.id = id;
 }
 
-
-apiCaller.prototype.performRequest = function (endpoint, data) {
+var performRequest = function (endpoint, data,reqCallback) 
+{
     var method = 'GET';
     var dataString = JSON.stringify(data);
-    var headers = {};
-
-    endpoint += '?' + querystring.stringify(data);
+    var headers = {'Accept':"application/json"
+					,'user-key':apiKey};
 
     var options = {
         host: host,
@@ -26,30 +25,38 @@ apiCaller.prototype.performRequest = function (endpoint, data) {
         method: method,
         headers: headers
     };
-
+		
     https.request(options, function(res) {
-        console.log("statusCode: ", res.statusCode);
+        console.log('statusCode: ', res.statusCode);
         res.setEncoding('utf-8');
-
+		
         var responseString = '';
 
         res.on('data', function(data) {
             responseString += data;
             console.info('GET result:\n');
-            process.stdout.write(d);
+			console.info(data);
+			reqCallback(data);
             console.info('\n\nCall completed');
         });
-
-        // res.on('end', function() {
-        //     console.log(responseString);
-        //     var responseObject = JSON.parse(responseString);
-        //     success(responseObject);
-        // });
+		
+		res.on('success', function(data) {
+            console.log('result' + data);
+			console.info('\n\nCall completed');
+        });
+		 
+		 res.on('error', function(e)
+    {
+        // Call callback function with the error object which comes from the response
+		console.log('this err:' + e);
+        callback(e, null);
     });
+    }).end();
+}
 
-     // req.write(dataString);
-     // req.end();
-
+var test = function()
+{
+	return 'test';
 }
 
 function login(){
@@ -62,3 +69,5 @@ function login(){
 }
 
 module.exports = apiCaller;
+module.exports.performRequest = performRequest;
+module.exports.test = test;
--- frontend/controller/hello.js
+++ frontend/controller/hello.js
@@ -7,16 +7,25 @@ var apiKey = 'c8c3348fe90d2d59d73cb8f4bde9c8f6';
 app.set('view engine', 'jade');
 app.set('views','../views');
 
-app.get('/', function (req, res) {
-
+app.get('/', function (req, res) 
+{
+	var result;
 	
-	res.render('index', { title: 'Hey', message: 'Hello there!'});  
+	apiCall.performRequest('/api/v2.1/cities?city_ids=2','',function(data){
+		res.render('index', { title: 'Hey', message: data});  
+	});
+	var tResult = apiCall.test();
+	
+	console.log('test result is: ' + tResult);
+	console.log('this is the result:' + result);
 });
 
-app.get('/who', function (req, res) {
+app.get('/who', function (req, res) 
+{
 	res.render('test', { title: 'Hey', message: 'Hello there!', about: 'whats going on'});
 });
 
-app.listen(3000, function () {
+app.listen(3000, function () 
+{
   console.log('Example app listening on port 3000!');
 });
